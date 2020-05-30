254f94f2e7605374ccffcce8ab6c64711cb1e5e6
Nicolas Jonas <nnico+gitlab@pm.me>
2020-05-30 13:40:20 +0200

    boom

--- google-webfont-downloader.php
+++ google-webfont-downloader.php
@@ -1,46 +1,94 @@
 #!/usr/bin/env php
 <?php
 
-$most_popular_n = 99999999;
+define( 'MOST_POPULAR', 99999 );
 
-$fonts = json_decode( shell_exec( 'curl https://google-webfonts-helper.herokuapp.com/api/fonts' ) );
+get_webfonts();
 
-$i = 1;
+function get_webfonts() {
 
-foreach ( $fonts as $font ) {
+	$fonts = json_decode( shell_exec( 'curl https://google-webfonts-helper.herokuapp.com/api/fonts' ) );
 
-	$url = "https://google-webfonts-helper.herokuapp.com/api/fonts/{$font->id}?download=zip&subsets=latin&formats=woff,woff2";
-	$url = escapeshellarg($url);
+	echo PHP_EOL . 'Font-count: ' . count( $fonts ) . PHP_EOL . PHP_EOL;
 
-	$folder_name = get_folder_name( $font->id );
+	$download_dir = 'downloads';
 
-	echo $folder_name . PHP_EOL;
-
-	shell_exec( "wget -N -q --output-document {$font->id}.zip $url" );
-	shell_exec( "mkdir -p {$folder_name}" );
-	shell_exec( "unzip -o {$font->id}.zip -d {$folder_name}" );
-	shell_exec( "rm {$font->id}.zip" );
-
-	if( $i > $most_popular_n ) {
-		break;
+	if ( ! file_exists($download_dir)) {
+		mkdir($download_dir, 0700);
 	}
-	$i++;
+
+	$i = 1;
+
+	foreach ( $fonts as $font ) :
+
+		foreach ( $font->subsets as $subset ) :
+
+			$url = "https://google-webfonts-helper.herokuapp.com/api/fonts/{$font->id}?download=zip&subsets={$subset}&formats=woff,woff2";
+			$url = escapeshellarg($url);
+
+			$font_id       = str_replace('-', '', $font->id);
+			$license       = detect_license( $font_id );
+			$license_txt   = getcwd() . "/$license/$font_id/LICENSE.txt";
+			$ofl_txt       = getcwd() . "/$license/$font_id/OFL.txt";
+			$webfont_dir   = getcwd() . "/webfonts/$license/$font_id/$subset";
+			$download_file = getcwd() . "/downloads/$font_id-$subset.zip";
+
+			echo $webfont_dir . PHP_EOL;
+
+			if ( is_file("$webfont_dir/LICENSE.txt") || is_file("$webfont_dir/OFL.txt") ) {
+				echo 'already ready' . PHP_EOL . PHP_EOL;
+				continue;
+			}
+			if ( ! file_exists($webfont_dir)) {
+				mkdir($webfont_dir, 0700, true);
+			}
+			if ( ! file_exists($download_file)) {
+				echo shell_exec( "wget -N -q --output-document $download_file $url" );
+			}
+
+			$zip = new ZipArchive();
+			$res = $zip->open($download_file);
+			if (true === $res) {
+				$zip->extractTo($webfont_dir);
+				$zip->close();
+			} else {
+
+				echo "Doh! I couldn't open $download_file" . PHP_EOL . PHP_EOL;
+
+				if ( file_exists($download_file)) {
+					echo "$download_file exists";
+					unlink($download_file);
+				} else {
+					echo "$download_file does not exists";
+				}
+				continue;
+			}
+
+			if ( is_file( $license_txt ) ) {
+				copy( $license_txt, "$webfont_dir/LICENSE.txt" );
+			}
+			if ( is_file( $ofl_txt ) ) {
+				copy( $ofl_txt, "$webfont_dir/OFL.txt" );
+			}
+
+		endforeach;
+
+		if ( $i > MOST_POPULAR ) {
+			break;
+		}
+		$i++;
+
+	endforeach;
 }
 
-function get_folder_name( $font_id ) {
-	$font_id_no_dash = str_replace('-', '', $font_id);
+function detect_license( $font_id ) {
 
-	foreach ( array( 'apache', 'ofl', 'ufl' ) as $value ) {
+	foreach ( array( 'apache', 'ofl', 'ufl' ) as $license ) {
 
-		if ( is_dir( getcwd() . "/{$value}/{$font_id_no_dash}" ) ) {
-			$folder_name = "{$value}/{$font_id_no_dash}/webfonts";
-			break;
+		if ( is_dir( getcwd() . "/{$license}/{$font_id}" ) ) {
+			return $license;
 		}
 	}
 
-	if ( empty( $folder_name ) ) {
-		$folder_name = "undetected/{$font_id}";
-	}
-
-	return $folder_name;
+	return 'undetected';
 }
