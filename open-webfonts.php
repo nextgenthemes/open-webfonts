36c0e4af48488c5d8f9a24bfa37aad5a773bbe38
Nicolas Jonas <nnico+git@pm.me>
2023-04-13 17:41:12 +0200

    Update 2023-04-13--17-41-10

--- open-webfonts.php
+++ open-webfonts.php
@@ -350,7 +350,7 @@ function curl_instance() {
 
 function download( string $url ): string {
 
-	$ua_win = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36';
+	$ua_win = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36';
 	$ua_mac = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36';
 
 	$ch = curl_instance();
